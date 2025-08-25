e59959bc49a43134348b9690819d292682c41c23
Mimpaguibe Mintoama <mimpaguibe3@gmail.com>
2025-08-26 00:26:43 +0100

    Update urls.py

--- connexion/urls.py
+++ connexion/urls.py
@@ -3,4 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.connexion, name = 'connexion'), 
+    path('inscription/', views.inscription, name = 'inscription'),
+    path('déconnexion/', views.logout, name='logout'),
+    path('article/', views.article, name='article'),
+    path('article/<int:id_article>',detail, name='detail'),
+    path('search/', views.search_view, name='search'),
 ]
